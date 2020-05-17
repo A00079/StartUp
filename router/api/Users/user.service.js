702003eb2ea8645da53e0244ec453c7a27f40ef5
A00079 <bendreajay007@gmail.com>
2020-05-17 06:47:20 -0700

    DB Fix

--- router/api/Users/user.service.js
+++ router/api/Users/user.service.js
@@ -2,7 +2,7 @@ const pool = require('../../../dbconfig/dbconfig');
 
 module.exports = {
     create : (data, callBack) =>{
-        let sql = 'INSERT INTO registeredusers (token) VALUES (?,?,?,?)';
+        let sql = 'INSERT INTO registeredusers (fullname,email,token,subscribed) VALUES (?,?,?,?)';
         pool.query(sql,[data.fullname,data.email,data.token,data.subscribed],(err,results,fields) =>{
             if(err){
                 return callBack(err)
