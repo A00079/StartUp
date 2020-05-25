6ad296ea813e58815bf35d770e8b815ba6ee9d18
A00079 <bendreajay007@gmail.com>
2020-05-25 16:35:25 +0530

    Fix in app Notification

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9501a592d5e6a0b528610aa3db6d70d9.js"
+  "/precache-manifest.c4ae7a4775c2c21a7301b2fe892c039f.js"
 );
 
 self.addEventListener('message', (event) => {
